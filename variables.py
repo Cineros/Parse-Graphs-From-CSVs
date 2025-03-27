df7d4f8ce49c39195c11af1ac55f22834d352b4c
Darius Wolfe <128618957+Cineros@users.noreply.github.com>
2025-03-27 18:43:20 -0400

    Changed name to app.py and adjusted paths

--- variables.py
+++ variables.py
@@ -4,18 +4,18 @@ damage_field_names = ["Parse %","Name","Amount","Ilvl","Ilvl %","Active","DPS","
 healing_field_names = ["Parse %","Name","Amount","Overheal","Ilvl","Ilvl %","Active","HPS",""] #These are the fields from the CSV files
 
 
-damage_path = r"C:\Parse-Graphs-From-CSVs\Damage CSVs"
-heal_path = r"C:\Parse-Graphs-From-CSVs\Healing CSVs" #These are the locations of the complete CSV files for a given stat.
+damage_path = r"Parse-Graphs-From-CSVs\Damage CSVs"
+heal_path = r"Parse-Graphs-From-CSVs\Healing CSVs" #These are the locations of the complete CSV files for a given stat.
 
 
-player_damage_path = r"C:\Parse-Graphs-From-CSVs\Player Damage CSVs"
-player_healing_path = r"C:\Parse-Graphs-From-CSVs\Player Healing CSVs" #Updated paths after the export to CSV function runs.
+player_damage_path = r"Parse-Graphs-From-CSVs\Player Damage CSVs"
+player_healing_path = r"Parse-Graphs-From-CSVs\Player Healing CSVs" #Updated paths after the export to CSV function runs.
 
-damage_csv_path = r"C:\Parse-Graphs-From-CSVs\Damage CSVs"
-healing_csv_path = r"C:\Parse-Graphs-From-CSVs\Healing CSVs"
+damage_csv_path = r"Parse-Graphs-From-CSVs\Damage CSVs"
+healing_csv_path = r"Parse-Graphs-From-CSVs\Healing CSVs"
 
-bad_logs_path = r"C:\Parse-Graphs-From-CSVs\Bad_Logs"
-export_path = r"C:\Parse-Graphs-From-CSVs"
+bad_logs_path = r"Parse-Graphs-From-CSVs\Bad_Logs"
+export_path = r"Parse-Graphs-From-CSVs"
 
 #A palette of colors used across multiple graphs in this program.
 color_palette = [
